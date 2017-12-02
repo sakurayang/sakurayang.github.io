b0557f2362293d982a113ec54eeef847ad7673cc
sakurayang <1304274443@qq.com>
2017-12-02 23:08:00 +0800

    Add files via upload

---
+++ load.js
@@ -0,0 +1,18 @@
+window.addEventListener("load", 
+			function() {
+				console.time("load");
+				console.log("Js loaded");
+				window.status="Loading.......";
+				console.log("Page loading");
+                var wrapper = document.getElementById("wrapper");
+				console.log("Delete overlay");
+                document.body.removeChild(wrapper);
+				document.title = 'Github List';
+				console.log("change Title");
+            }
+			);
+			function loaded() {
+			window.status="page loade finish!";
+			console.log("page load finish!");
+			console.timeEnd("load");
+			}
