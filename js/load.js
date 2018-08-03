b37946200a90eb7fb13d28d69c2c628abcd6d442
sakurayang <1304274443@qq.com>
2018-08-03 22:24:24 +0800

    检测桌面颜色（滑稽）

--- js/load.js
+++ js/load.js
@@ -1,19 +1,33 @@
 function load (title_name){
-window.addEventListener("load", 
-			function() {
-				console.time("load");
-				console.log("Js loaded");
-				window.status="Loading.......";
-				console.log("Page loading");
-                var wrapper = document.getElementById("wrapper");
-				console.log("Delete overlay");
-                document.body.removeChild(wrapper);
-				document.title = title_name;
-				console.log("change Title");
-            }
-			);}
-			function loaded() {
-			window.status="page loade finish!";
-			console.log("page load finish!");
-			console.timeEnd("load");
+	window.addEventListener("load", 
+		function() {
+			console.time("load");
+			console.log("Js loaded");
+			window.status="Loading.......";
+			console.log("Page loading");
+            var wrapper = document.getElementById("wrapper");
+			console.log("Delete overlay");
+            document.body.removeChild(wrapper);
+			document.title = title_name;
+			console.log("change Title");
+        }
+	);
+}
+function loaded() {
+	window.status="page loade finish!";
+	console.log("page load finish!");
+	console.timeEnd("load");
+	if (confirm("是否将主题颜色切换为您的桌面颜色")){
+		alert("检测不到您的桌面颜色，请检查您的桌面是否提供API");
+		changeColor();
+		function changeColor(){
+			var color = prompt("请手动输入颜色值","rbga(255 , 255 , 255 , 0.8)");
+			if (color != null && color !=''){
+				document.body.style.backgroundColor=color;
+			} else {
+				alert("请检查您的输入");
+				changeColor();
 			}
+		}
+	}
+}
